a2d9502158ccf671ddc0844836644a21c5827998
MathiasGD <103475378+MathiasGD@users.noreply.github.com>
2025-11-11 15:52:40 -0300

    Integrate MUI components and Emotion for styling; update theme configuration and menu navigation buttons. Add new dependencies for MUI and Emotion in package.json and package-lock.json.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,51 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#7A1C52',
+      light: '#980A68',
+      dark: '#680747',
+      contrastText: '#FFF3EA',
+    },
+    text: {
+      primary: '#141010',     // preto
+      secondary: '#2B2725',   // preto-fraco
+    },
+  },
+  typography: {
+    fontFamily: '"Outfit", sans-serif',
+    button: {
+      fontFamily: '"Outfit", sans-serif',
+      fontWeight: 400,
+      textTransform: 'none',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 2,
+          padding: '8px 40px',
+        },
+        outlined: {
+          borderColor: '#7A1C52',
+          '&:hover': {
+            backgroundColor: 'rgba(122, 28, 82, 0.04)',
+          },
+        },
+      },
+    },
+  },
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeProvider theme={theme}>
       <App />
+    </ThemeProvider>
   </StrictMode>,
 )
--- src/pages/menu/index.tsx
+++ src/pages/menu/index.tsx
@@ -1,11 +1,22 @@
+import { Button } from "@mui/material";
 import logo from "../../assets/logo-licor.svg";
+import { useNavigate } from "react-router-dom";
 
 function Menu() {
+  const navigate = useNavigate();
+
+  const handleNavigate = (path: string) => {
+    navigate(path);
+  }
+
   return (
     <div className="flex flex-col items-center justify-center gap-9">
       <img src={logo} alt="Logo" className="w-16 h-16" />
-      <h1>Menu</h1>
-      <p>Bem-vindo à página de menu!</p>
+      <div className="flex gap-6">
+        <Button variant="contained" color="primary" onClick={() => handleNavigate("/estoque")} disableElevation>Estoque</Button>
+        <Button variant="contained" color="primary" onClick={() => handleNavigate("/pedidos")} disableElevation>Pedidos</Button>
+        <Button variant="contained" color="primary" onClick={() => handleNavigate("/drinks")} disableElevation>Drinks</Button>
+      </div>
     </div>
   );
 }
