1047f018d46bf8c76a5fbb0285dde0ae381084f4
MathiasGD <103475378+MathiasGD@users.noreply.github.com>
2025-11-14 14:34:42 -0300

    Add @mui/x-data-grid dependency and implement drinks management page with modal for drink creation; enhance modal component to support right-side content for ingredient composition.

--- src/profissional/components/modal.tsx
+++ src/profissional/components/modal.tsx
@@ -1,19 +1,19 @@
 import { motion, AnimatePresence } from 'motion/react';
 
 
-const Modal = ({ children, isOpen }: { children: React.ReactNode, isOpen: boolean }) => {
+const Modal = ({ children, isOpen, rightSide }: { children: React.ReactNode, isOpen: boolean, rightSide?: React.ReactNode }) => {
   return (
     <AnimatePresence>
       {isOpen && (
         <motion.div 
-          className="fixed inset-0 bg-preto/50 flex items-end justify-center"
+          className="fixed inset-0 bg-preto/50 flex items-end justify-center gap-4"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.2 }}
         >
           <motion.div
-            className="h-5/6 bg-branco p-5 w-114"
+            className="h-5/6 bg-branco p-5 w-114 overflow-auto"
             initial={{ y: '100%' }}
             animate={{ y: 0 }}
             exit={{ y: '100%' }}
@@ -21,6 +21,18 @@ const Modal = ({ children, isOpen }: { children: React.ReactNode, isOpen: boolea
           >
             {children}
           </motion.div>
+          {rightSide && 
+            <motion.div
+              className="h-4/6 bg-branco p-5 w-114 overflow-auto"
+              initial={{ y: '100%' }}
+              animate={{ y: 0 }}
+              exit={{ y: '100%' }}
+              transition={{ duration: 0.2, ease: 'easeOut' }}
+            >
+              {rightSide}
+            </motion.div>
+          }
+          
         </motion.div>
       )}
     </AnimatePresence>
--- src/profissional/pages/drinks/index.tsx
+++ src/profissional/pages/drinks/index.tsx
@@ -1,8 +1,166 @@
+import { Button, MenuItem, Select, TextField } from "@mui/material";
+import type { Drink, Ingrediente } from "../../../services/pedidos";
+import { DataGrid, type GridColDef } from "@mui/x-data-grid";
+import { useState } from "react";
+import Modal from "../../components/modal";
+
+const drinks: Drink[] = [
+  {
+    id: 1,
+    nome: "Drink base",
+    modoPreparo: "Modo de preparo do drink base",
+    precoBase: 34.9,
+    descricao: "Descrição do drink base",
+    composicao: [],
+  },
+  {
+    id: 2,
+    nome: "Drink de limão",
+    modoPreparo: "Modo de preparo do drink de limão",
+    precoBase: 39.9,
+    descricao: "Descrição do drink de limão",
+    composicao: [],
+  },
+];
+
+const colunas: GridColDef[] = [
+  {
+    field: "nome",
+    headerName: "Nome",
+    width: 200,
+    flex: 1,
+  },
+  {
+    field: "descricao",
+    headerName: "Descrição",
+    width: 300,
+    flex: 1,
+  },
+  {
+    field: "precoBase",
+    headerName: "Preço base",
+    width: 120,
+    flex: 1,
+  },
+]
+
+const ingredientes: Ingrediente[] = [
+  {
+    id: 1,
+    nome: "Ingrediente 1",
+    descricao: "Descrição do ingrediente 1",
+    estoque: {
+      id: 1,
+      dataAtualizado: new Date(),
+      quantidadeDisponivel: 100,
+    },
+  },
+  {
+    id: 2,
+    nome: "Ingrediente 2",
+    descricao: "Descrição do ingrediente 2",
+    estoque: {
+      id: 2,
+      dataAtualizado: new Date(),
+      quantidadeDisponivel: 100,
+    },
+  },
+]
+
 function Drinks() {
+  const [openModalCadastro, setOpenModalCadastro] = useState(false);
+  const [composicao, setComposicao] = useState<{unidadeMedida: string, quantidade: number | '', ingredienteId: number | ''}[]>([{ingredienteId: '', unidadeMedida: "", quantidade: '', }]);
+
+  const adicionarIngrediente = () => {
+    setComposicao([...composicao, { ingredienteId: '', unidadeMedida: "", quantidade: '',  }]);
+  };
+
+  const atualizarIngrediente = (index: number, campo: 'ingredienteId' | 'quantidade' | 'unidadeMedida', valor: number | string) => {
+    const novaComposicao = [...composicao];
+    novaComposicao[index] = { ...novaComposicao[index], [campo]: valor };
+    setComposicao(novaComposicao);
+  };
+  
   return (
-    <div>
-      <h1>Drinks</h1>
-      <p>Bem-vindo à página de drinks!</p>
+    <div className="flex flex-col gap-4 w-full items-start">
+      <Button variant="contained" disableElevation onClick={() => setOpenModalCadastro(true)}>Cadastrar drink</Button>
+        <DataGrid className="w-full"
+          sx={{
+            "& .MuiDataGrid-columnHeader": {
+              backgroundColor: "#F9ECE2",
+            },
+            "& .MuiDataGrid-row": {
+              backgroundColor: "#FFF3EA",
+            },
+            "& .MuiDataGrid-row:hover": {
+              backgroundColor: "#F9ECE2",
+              cursor: "pointer",
+            },
+            "& .MuiDataGrid-footerContainer": {
+              borderTop: "1px solid #E8D5C4",
+              backgroundColor: "#FFF3EA",
+            },
+            border: "none",
+          }}
+          rows={drinks}
+          columns={colunas}
+          rowSelection={false} />
+      <Modal isOpen={openModalCadastro} 
+        rightSide={
+          <div className="flex flex-col gap-3">
+            <p className="text-xl font-medium self-center">Composição:</p>
+            {composicao.map((item, index) => (
+              <div key={index} className="flex flex-col gap-3">
+                <div className="flex flex-col gap-1">
+                  <p className="text-sm">Ingrediente:</p>
+                  <Select size="small" value={item.ingredienteId} onChange={(e) => atualizarIngrediente(index, 'ingredienteId', e.target.value)}>
+                    {ingredientes.map((ingrediente) => (
+                      <MenuItem key={ingrediente.id} value={ingrediente.id}>{ingrediente.nome}</MenuItem>
+                    ))}
+                  </Select>
+                </div>
+                <div className="flex gap-2">
+                    <TextField
+                      size="small"
+                      placeholder="Unidade de medida"
+                      value={item.unidadeMedida}
+                      onChange={(e) => atualizarIngrediente(index, 'unidadeMedida', e.target.value)}
+                      sx={{ flex: 1 }}
+                    />
+                    <TextField
+                      size="small"
+                      type="number"
+                      placeholder="Quantidade"
+                      value={item.quantidade}
+                      onChange={(e) => atualizarIngrediente(index, 'quantidade', e.target.value)}
+                      sx={{ flex: 1 }}
+                    />
+                  </div>
+              </div>
+            ))}
+            <Button variant="contained" disableElevation onClick={adicionarIngrediente}>Adicionar ingrediente</Button>
+          </div>
+        }
+      >
+        <div className="flex flex-col justify-between gap-3 min-h-full">
+          <div className="flex flex-col gap-3">
+            <p className="text-xl font-medium text-roxo-escuro self-center">Novo drink</p>
+            <TextField fullWidth placeholder="Nome" />
+            <div className="w-full h-px bg-branco-muito-escuro" />
+            <TextField fullWidth placeholder="Preço" />
+            <div className="w-full h-px bg-branco-muito-escuro" />
+            <TextField fullWidth placeholder="Descrição" />
+            <div className="w-full h-px bg-branco-muito-escuro" />
+            <p className="text-sm">Composição:</p>
+            <div className="w-full h-px bg-branco-muito-escuro" />
+            <TextField fullWidth placeholder="Modo de preparo" />
+          </div>
+          <div className="flex flex-col gap-2 w-full">
+            <Button variant="outlined" disableElevation onClick={() => setOpenModalCadastro(false)}>Cancelar</Button>
+            <Button variant="contained" disableElevation onClick={() => setOpenModalCadastro(false)}>Cadastrar</Button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
